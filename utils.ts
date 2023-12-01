b815275ce7cf5c7990b77ff6bf845cb8fa2f3a0e
Nailuj <nailuj29gaming@gmail.com>
2023-12-01 18:13:00 -0500

    Formatting

--- utils.ts
+++ utils.ts
@@ -5,7 +5,7 @@ export const loadData = async (day: number) => {
     return decoder.decode(data)
 }
 
-export const loadTestData = async (day: number, index:any = "") => {
+export const loadTestData = async (day: number, index: any = "") => {
     const decoder = new TextDecoder("utf-8")
     const data = await Deno.readFile(`test${index}_day${day}.dat`)
 
