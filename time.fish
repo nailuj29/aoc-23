c1e459e6bc174e403d82134027685754bc1eae25
Nailuj <nailuj29gaming@gmail.com>
2023-12-09 00:31:32 -0500

    Improve times script

--- time.fish
+++ time.fish
@@ -1,6 +1,8 @@
 #! /usr/bin/fish
 
-for i in (seq 1 9)
-    echo Day $i
-    time deno run --allow-read day$i.ts > /dev/null
+for i in (seq 1 25)
+    if test -e day$i.ts
+        echo Day $i
+        time deno run --allow-read day$i.ts > /dev/null
+    end
 end
